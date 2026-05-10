f469ce2c6f501d5207221635222b36f2ffdaaa0a
molomalteser-org <molomalteser@gmail.com>
2026-05-10 16:41:06 +0200

    Create pins_arduino.h

---
+++ boards/CyberPi/pins_arduino.h
@@ -0,0 +1,21 @@
+#ifndef Pins_Arduino_h
+#define Pins_Arduino_h
+
+#include <stdint.h>
+
+static const uint8_t TX = 1;
+static const uint8_t RX = 3;
+
+static const uint8_t SDA = 21;
+static const uint8_t SCL = 22;
+
+static const uint8_t SS = 5;
+static const uint8_t MOSI = 23;
+static const uint8_t MISO = 19;
+static const uint8_t SCK = 18;
+
+// CyberPi spezifisch
+#define DEEPSLEEP_WAKEUP_PIN 36 
+#define DEEPSLEEP_PIN_ACT LOW
+
+#endif
