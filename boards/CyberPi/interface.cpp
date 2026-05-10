5474d4c720e70d74302b1435d064021fb97df318
molomalteser-org <molomalteser@gmail.com>
2026-05-10 17:42:52 +0200

    Update interface.cpp

--- boards/CyberPi/interface.cpp
+++ boards/CyberPi/interface.cpp
@@ -3,95 +3,119 @@
 #include <Arduino.h>
 #include <interface.h>
 #include <globals.h>
+#include <Wire.h>
 
-// CyberPi nutzt den AW9523 Expander für fast alles
+// CyberPi nutzt den AW9523 Expander für Joystick und Backlight
 #include "HAL/io_expander/io_expander.h"
-
 extern io_expander io_ext; 
 
 /***************************************************************************************
 ** Function name: _setup_gpio()
-** Description:   Initiales Setup für CyberPi Hardware
+** Description:   Initiales Setup der CyberPi Hardware
 ***************************************************************************************/
 void _setup_gpio() {
-    // Initialisiere den I2C Bus für den AW9523
-    Wire.begin(I2C_SDA, I2C_SCL);
-    
+    // I2C für den AW9523 starten
+    Wire.begin(GROVE_SDA, GROVE_SCL);
+
+    // Initialisiere den Expander
     if (!io_ext.init(IO_EXPANDER_ADDR, &Wire)) {
         Serial.println("CyberPi AW9523 nicht gefunden!");
     } else {
-        Serial.println("CyberPi I/O Expander gestartet.");
-        // Backlight Pin am Expander als Output definieren
+        // Backlight Pin am Expander als Ausgang (CyberPi Pin 1)
         io_ext.setPinDirection(1, OUTPUT); 
     }
+
+    // SPI CS Pins für interne Module auf HIGH (deaktiviert) setzen
+    pinMode(TFT_CS, OUTPUT);
+    digitalWrite(TFT_CS, HIGH);
+    pinMode(SDCARD_CS, OUTPUT);
+    digitalWrite(SDCARD_CS, HIGH);
 
     bruceConfig.colorInverted = 0;
 }
 
 /***************************************************************************************
-** Function name: _post_setup_gpio()
-** Description:   Aktiviert das Display-Backlight
+** Function name: getBattery()
+** Description:   CyberPi hat meist keinen Gauge-Chip, daher Dummy-Wert oder ADC
 ***************************************************************************************/
-void _post_setup_gpio() {
-    // CyberPi Backlight einschalten (Pin 1 am AW9523)
-    io_ext.turnPinOnOff(1, HIGH);
-    Serial.println("Backlight aktiv.");
+int getBattery() {
+    return 100; // CyberPi hängt meist am USB, daher "voll"
+}
+
+bool isCharging() {
+    return true; // Per Default true, da USB-betrieben
 }
 
 /*********************************************************************
 ** Function: _setBrightness
-** Description: Steuert die Helligkeit über den Expander
+** Description: Steuert das Licht über die PWM-Funktion des AW9523
 **********************************************************************/
 void _setBrightness(uint8_t brightval) {
-    // Da der AW9523 auch analogWrite (PWM) kann, nutzen wir das für das Backlight
-    // brightval kommt in 0-100%, wir rechnen um auf 0-255
-    uint8_t pwm_val = (brightval * 255) / 100;
-    
-    // Pin 1 ist am CyberPi oft das Backlight
-    io_ext.analogWrite(1, pwm_val);
+    if (brightval == 0) {
+        io_ext.turnPinOnOff(1, LOW);
+    } else {
+        // Umrechnung 0-100% in 0-255 PWM
+        uint8_t pwm_val = (brightval * 255) / 100;
+        io_ext.analogWrite(1, pwm_val);
+    }
 }
 
 /*********************************************************************
 ** Function: InputHandler
-** Description: Liest den CyberPi Joystick über den AW9523 aus
+** Handles: Up, Down, Left, Right, Select via AW9523 Expander
 **********************************************************************/
 void InputHandler(void) {
-    static long d_tmp = 0;
-    if (millis() - d_tmp > 150) { // Debounce / Verzögerung
-        
-        // Wir lesen die Pins vom Expander (definiert in der platformio.ini)
-        bool up    = !io_ext.readPin(IO_EXP_UP);    // Low-Aktiv
-        bool down  = !io_ext.readPin(IO_EXP_DOWN);
-        bool left  = !io_ext.readPin(IO_EXP_LEFT);
-        bool right = !io_ext.readPin(IO_EXP_RIGHT);
-        bool sel   = !io_ext.readPin(IO_EXP_SEL);
+    static unsigned long tm = 0;
+    if (millis() - tm < 200 && !LongPress) return;
 
-        if (up || down || left || right || sel) {
-            if (wakeUpScreen()) {
-                d_tmp = millis();
-                return; 
-            }
-            
-            AnyKeyPress = true;
-            PrevPress = up;
-            NextPress = down;
-            EscPress  = left;
-            SelPress  = sel;
-            // 'right' könnte für weitere Funktionen wie Page-Switch genutzt werden
-            
-            d_tmp = millis();
-        }
+    // Pins vom AW9523 lesen (die Pins sind in deiner pins_arduino.h definiert)
+    bool _u = io_ext.readPin(IO_EXP_UP);    // Joystick hoch
+    bool _d = io_ext.readPin(IO_EXP_DOWN);  // Joystick runter
+    bool _l = io_ext.readPin(IO_EXP_LEFT);  // Joystick links
+    bool _r = io_ext.readPin(IO_EXP_RIGHT); // Joystick rechts
+    bool _s = io_ext.readPin(IO_EXP_SEL);   // Joystick Druck (Select)
+
+    // Logik: AW9523 Pins sind meist Pull-Up (LOW wenn gedrückt)
+    if (!_u || !_d || !_l || !_r || !_s) {
+        tm = millis();
+        if (!wakeUpScreen()) AnyKeyPress = true;
+        else return;
+
+        if (!_l) PrevPress = true;
+        if (!_r) NextPress = true;
+        if (!_u) { UpPress = true; PrevPagePress = true; }
+        if (!_d) { DownPress = true; NextPagePress = true; }
+        if (!_s) SelPress = true;
     }
 }
 
 /*********************************************************************
 ** Function: powerOff
-** Description: Tiefschlaf für den CyberPi
+** Description: Schaltet das Licht aus und geht in den DeepSleep
 **********************************************************************/
 void powerOff() {
-    // Schalte Display-Licht aus bevor wir schlafen
     io_ext.turnPinOnOff(1, LOW);
     esp_deep_sleep_start();
 }
 
-void checkReboot() {}
+/*********************************************************************
+** Function: checkReboot
+** Description: Hält man "Links" gedrückt, wird ein Power-Off simuliert
+**********************************************************************/
+void checkReboot() {
+    if (io_ext.readPin(IO_EXP_LEFT) == LOW) { // Joystick links als "ESC" Ersatz
+        uint32_t time_count = millis();
+        while (io_ext.readPin(IO_EXP_LEFT) == LOW) {
+            if (millis() - time_count > 500) {
+                int countDown = (millis() - time_count) / 1000 + 1;
+                if (countDown < 3) {
+                    tft.drawCentreString("OFF IN " + String(countDown) + "/2", tftWidth / 2, 12, 1);
+                } else {
+                    powerOff();
+                }
+                delay(10);
+            }
+        }
+        tft.fillRect(0, 12, tftWidth, 10, bruceConfig.bgColor);
+    }
+}
